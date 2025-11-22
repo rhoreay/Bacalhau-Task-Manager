3f755c0910b74b324c6cd4a076c9536e4f55f8a6
Rafael Horeay <devrafaelhoreay@gmail.com>
2025-11-21 21:06:30 -0300

    fix task_observer

--- app/observers/task_observer.rb
+++ app/observers/task_observer.rb
@@ -1,5 +1,5 @@
 class TaskObserver
-    def sef.after_create(task)
+    def self.after_create(task)
         task.people.each do |person|
             puts ">>Enviando email ao #{person.name} sobre a tarefa #{task.title}"
         end
